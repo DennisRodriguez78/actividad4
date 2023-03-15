885e10ffe014b0a4ddd0bde821aa6633319890ed
unknown <dennisrodriguez771@gmail.com>
2023-03-14 23:24:07 -0600

    Primer commit

---
+++ codigo 1.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    //codigo 1
+    /*int i=0;
+    for(i=5;i<8; i+=3){
+        cout<<i<<endl;
+    }
+    cout<<"i fuera"<<i<<endl;
+    system("pause");*/
+    
+    //codigo 2
+    /*for(i=5;i<8; i+=3){
+        if (i==5)
+        {
+           break;
+           cout<<"if"<<endl;
+        }
+        
+        cout<<i<<endl;
+    }
+    cout<<"fuera del for"<<i<<endl;
+    system("pause");*/
+ 
+    //codigo 3
+    /*for(i=5;i<8; i+=3){
+        if (i==5)
+        {
+           cout<<"if"<<endl;
+           continue;
+        }
+        
+        cout<<i<<endl;
+    }
+    cout<<"fuera del for"<<i<<endl;
+    system("pause");*/
+
+     //codigo 4
+    /*for(i=5;i<8; i+=3){
+        if (i==5)
+        {
+           cout<<"if"<<endl;
+           continue;
+        }
+        
+        cout<<i<<endl;
+    }
+    cout<<"fuera del for"<<i<<endl;
+    system("pause");*/
+    //codigo 5
+    /*int datos[5]={10,40,100,250,3000};
+    for(int i=0;i<5;i++){
+            cout<<datos[i]<<endl;
+    }
+    system("pause");*/
+     //codigo 6
+    /*int datos[]={8,12,35,200,1000};   //array initialization
+ 	//cout<<sizeof(datos)<<endl;
+ 	//cout<<sizeof(datos[0])<<endl;
+    int tama = (sizeof(datos)/sizeof(datos[0]));
+    
+    for (int i=0;i<tama;i++){
+    	cout<<datos[i]<<endl;
+	}*/
+     //codigo 7
+    /*int inicio=0,fin=0,multiplicar=0;
+cout <<"Ingrese tabla inicio:";
+cin>>inicio;
+cout <<"Ingrese tabla fin:";
+cin>>fin;
+for (int i=inicio; i<=fin;i++){
+	//cout<<i<<endl;
+	for (int ii=1; ii<=10;ii++){
+		//cout<<ii<<endl;
+	 multiplicar=i*ii;
+	 cout<<i<<" X "<<ii<<" = "<<multiplicar<<endl;
+	}
+	cout<<endl;
+}
+ 
+system("pause");*/
+
+
+}
---
+++ codigo 2.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char const *argv[])
+{
+    
+   /*int i=0;
+	
+	while(i<5){
+	
+		i++;
+		cout<<i<<endl;
+	
+	}
+	int dec=10;
+	cout<<"___________________"<<endl;
+	while(dec>0){
+		cout<<dec<<endl;
+	}
+    
+system("pause");*/
+	
+
+/*int i=0;
+    while (i>5)
+    {
+         cout<<"while"<<i<<endl;
+         i++;
+    }
+    int ii=0;
+    do
+    {
+        ii++;
+        cout<<"do while"<<ii<<endl;
+    } while (ii<11);*/
+    
+    
+	
+/*char respuesta;
+do
+{
+    cout<<"desea ingresar otro valor(s/n)"<<endl;
+    cin>>respuesta;
+} while (respuesta=='s' || respuesta =='S');*/
+
+
+
+
+
+
+
+
+    return 0;
+}
